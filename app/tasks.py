8c730c13426c6ecdee62daeea835ca2f9a440225
AdrianPayne <adriansacristan1993@gmail.com>
2025-03-30 22:45:59 +0100

    🎨 pre-commit, ci and formatting

--- app/tasks.py
+++ app/tasks.py
@@ -1,14 +1,17 @@
-from celery import Celery
 import os
+
+from celery import Celery
 
 celery_app = Celery(
     "worker",
     broker=os.getenv("CELERY_BROKER_URL", "redis://redis:6379/0"),
 )
 
+
 @celery_app.task
 def ping_task():
     return "pong"
+
 
 @celery_app.task
 def scheduled_hello():
